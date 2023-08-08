d0509ed8553dddb80d626d60978be188d55d2438
Luna <eskw0701@naver.com>
2023-08-09 00:45:30 +0900

    틀만 잡아두기

---
+++ API_Project/API_Project/ImageFeed/Cell/FeedCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCollectionViewCell.swift
+//  API_Project
+//
+//  Created by t2023-m0050 on 2023/08/09.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageViewFeed: UIImageView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ API_Project/API_Project/ImageFeed/ImageFeedViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ImageFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
