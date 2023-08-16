e0b74a00c11340a4066dc24d01c12e2020a5d9a3
Luna828 <eskw0701@naver.com>
2023-08-16 14:33:00 +0900

    [API_Project] Alamofire request/Response 부분 내용 추가

--- API_Project/API_Project/AlamofirePractice/FotoDataManager.swift
+++ API_Project/API_Project/AlamofirePractice/FotoDataManager.swift
@@ -1,12 +1,25 @@
-//
-//  FotoDataManager.swift
-//  API_Project
-//
-//  Created by t2023-m0050 on 2023/08/16.
-//
+/*
+let url = "https://어쩌구저쩌구"
+
+AF.request(url, // 첫번째 파라미터 요청할 url 담는다
+           method: .get, // 어떤 request method정하기
+           parameters: nil, // request body 전달 값
+           encoding: URLEncoding.default, //인코딩 방식 정하기
+           headers: ["Content-Type":"application/json", "Accept":"application/json"])
+    .validate(statusCode: 200..<300) //유효성 검사
+    .responseJSON { response in // 응답 Json 데이터
+
+    /** 서버로부터 받은 데이터 활용 */
+    switch response.result {
+    case .success(let data):
+        /** 정상적으로 reponse를 받은 경우 */
+    case .failure(let error):
+        /** 그렇지 않은 경우 */
+    }
+}
+*/
 
 import Alamofire
-
 //클라이언트와 서버를 연결해주는 것
 class FotoDataManager {
     func fotoDataManager(_ parameters: FotoAPIInput, _ viewController: ImageFeedViewController) {
